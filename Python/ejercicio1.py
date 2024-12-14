d2b244eb23229391de0de6d9c123f2b8872e17c0
Francini MS <francinimendezsalas@gmail.com>
2024-12-13 19:37:06 -0600

    Corrección del código duplicado y cierre de paréntesis

--- Python/ejercicio1.py
+++ Python/ejercicio1.py
@@ -4,13 +4,5 @@ libros = {
     "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género": "Ficción", "calificación": 4.3}
 }
 
-
-libros = {
-    "1984": {"autor": "George Orwell", "año": 1949, "género": "Distopía", "calificación": 4.8},
-    "To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género": "Ficción", "calificación": 4.9},
-    "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género": "Ficción", "calificación": 4.3}
-}
-
-
 libros["1984"]["calificación"] = 5.0
-print(f"Nueva calificación de '1984': {libros['1984']['calificación']}"
+print(f"Nueva calificación de '1984': {libros['1984']['calificación']}")
